e1d59500f069f810a6f483bd2b5879f419216294
Ananthu M <ananthum0813@gmail.com>
2026-02-26 10:18:03 +0530

    feat: add arithmetic operation endpoints and tests

--- task2/operations/urls.py
+++ task2/operations/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from operations.views import RegisterView, LoginView,LogoutView
+from operations.views import AddView,SubtractView,MultiplyView,DivideView
 
 
 urlpatterns = [
@@ -10,4 +11,10 @@ urlpatterns = [
     path("api/login/",         LoginView.as_view()),
     path("api/logout/",        LogoutView.as_view()),
     path("api/token/refresh/", TokenRefreshView.as_view()),
+
+     # Arithmetic (JWT required)
+    path("api/add/",           AddView.as_view()),
+    path("api/subtract/",      SubtractView.as_view()),
+    path("api/multiply/",      MultiplyView.as_view()),
+    path("api/divide/",        DivideView.as_view()),
 ]
