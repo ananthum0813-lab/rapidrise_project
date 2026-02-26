915100a8405a892de46444363b1f30f4525a66ed
Ananthu M <ananthum0813@gmail.com>
2026-02-26 10:54:57 +0530

    Revert "feat: add arithmetic operation endpoints and tests"
    This reverts commit e1d59500f069f810a6f483bd2b5879f419216294.

--- task2/operations/urls.py
+++ task2/operations/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from operations.views import RegisterView, LoginView,LogoutView
-from operations.views import AddView,SubtractView,MultiplyView,DivideView
 
 
 urlpatterns = [
@@ -11,10 +10,4 @@ urlpatterns = [
     path("api/login/",         LoginView.as_view()),
     path("api/logout/",        LogoutView.as_view()),
     path("api/token/refresh/", TokenRefreshView.as_view()),
-
-     # Arithmetic (JWT required)
-    path("api/add/",           AddView.as_view()),
-    path("api/subtract/",      SubtractView.as_view()),
-    path("api/multiply/",      MultiplyView.as_view()),
-    path("api/divide/",        DivideView.as_view()),
 ]
